df61a91c09b3a30baa87a4bc21b05a1226c6edf2
ferbjava <michal.kotecki@capgemini.com>
2018-07-20 21:47:00 +0200

    First commit

---
+++ src/QuadraticEquationCalculator/QuadraticEquationCalculator.java
@@ -0,0 +1,13 @@
+package QuadraticEquationCalculator;
+
+public class QuadraticEquationCalculator {
+
+	public QuadraticEquationCalculator(){
+		
+	}
+	
+	public void solver (double a, double b, double c){
+		
+	}
+	
+}
